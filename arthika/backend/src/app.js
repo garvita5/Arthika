2222f8242ae9d77e8ef3e51be9b08cdd5a084934
garvita5 <garvitakataria@gmail.com>
2025-07-19 03:11:08 +0530

    directory change

--- arthika/backend/src/app.js
+++ arthika/backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const corsOptions = {
   origin: [
     process.env.FRONTEND_URL || "http://localhost:5173",
-    process.env.PRODUCTION_URL || "https://arthika.vercel.app"
+    process.env.PRODUCTION_URL || "https://arthika.onrender.com"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
